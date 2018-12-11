61d4285bd95bb5780430e08d9d232e8231527a4f
austin <austin89peterson@gmail.com>
2018-12-11 09:55:38 -0700

    solved

---
+++ rna-transcription.js
@@ -0,0 +1,24 @@
+
+const mapObj = {
+    C: 'G',
+    G: 'C',
+    A: 'U',
+    T: 'A'
+}
+
+export const toRna = (rna) => {
+    
+    if(rna === 'U')
+    throw new Error('Invalid input DNA.')
+
+    rna = rna.replace(/C|G|A|T/g, function(matched){
+    return mapObj[matched]})
+    
+    if (rna.indexOf('X') !== -1)
+    throw new Error('Invalid input DNA.')
+    
+    else
+    return rna
+
+    }
+
